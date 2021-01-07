532563372b6b39ed9fbd176e0b30a5c06ce0b3fa
Vincent Texier <vit@free.fr>
2021-01-07 12:16:30 +0100

    Add Zeebe mapping of inputs/ouputs variables with sFEEL expression parsing
    WARNING: python sFEEL expression syntax differs from Zeebe FEEL expressions
    See https://pysfeel.readthedocs.io/en/latest/index.html

--- adhesive/execution/ExecutionTask.py
+++ adhesive/execution/ExecutionTask.py
@@ -1,5 +1,8 @@
 import logging
+import re
 from typing import Callable, Optional
+
+import pySFeel
 
 from adhesive.execution import token_utils
 from adhesive.execution.ExecutionBaseTask import ExecutionBaseTask, ExpressionList, RegexList
@@ -12,6 +15,7 @@ class ExecutionTask(ExecutionBaseTask):
     """
     A task implementation.
     """
+
     def __init__(self,
                  *args,
                  code: Callable,
@@ -21,7 +25,7 @@ class ExecutionTask(ExecutionBaseTask):
                  when: Optional[str] = None,
                  lane: Optional[str] = None,
                  deduplicate: Optional[str] = None,
-             ) -> None:
+                 ) -> None:
         """
         Create a new adhesive task. The `loop`, `when` and `lane` are only
         available when doing a programmatic API.
@@ -62,14 +66,50 @@ class ExecutionTask(ExecutionBaseTask):
                                              event.context.task.type)
             else:
                 params = token_utils.matches(self.re_expressions,
-                                         event.context.task_name)
+                                             event.context.task_name)
+
+            # inputs mapping
+            if event.task.mapping:
+                parser = pySFeel.SFeelParser()
+                for input in event.task.mapping["inputs"]:
+                    # remove zeebe equal
+                    source = input["source"].strip("= ")
+                    # replace variables by their values
+                    for variable, value in event.context.data.as_dict().items():
+                        if isinstance(value, str) or isinstance(value, int) or isinstance(value, float):
+                            # replace variable occurrence by value if not in quotes
+                            source = re.sub('{text}(?=([^"]*"[^"]*")*[^"]*$)'.format(text=variable),
+                                            value if not isinstance(value, str) else f"\"{value}\"", source)
+                    # parse source FEEL expression and put result in target variable
+                    status, result = parser.sFeelParse(source)
+                    if "errors" in status:
+                        raise Exception(",".join(status["errors"]))
+                    event.context.data[input["target"]] = result
 
             self.code(event.context, *params)  # type: ignore
 
+            # outputs mapping
+            if event.task.mapping:
+                parser = pySFeel.SFeelParser()
+                for output in event.task.mapping["outputs"]:
+                    # remove zeebe equal
+                    source = output["source"].strip("= ")
+                    # replace variables by their values
+                    for variable, value in event.context.data.as_dict().items():
+                        if isinstance(value, str) or isinstance(value, int) or isinstance(value, float):
+                            # replace variable occurrence by value if not in quotes
+                            source = re.sub('{text}(?=([^"]*"[^"]*")*[^"]*$)'.format(text=variable),
+                                            value if not isinstance(value, str) else f"\"{value}\"", source)
+                    # parse source FEEL expression and put result in target variable
+                    status, result = parser.sFeelParse(source)
+                    if "errors" in status:
+                        raise Exception(",".join(status["errors"]))
+                    event.context.data[output["target"]] = result
             # loop output
             if event.context.loop:
                 # add loop output element variable value to loop output collection dict
-                event.context.data[event.task.loop.output_collection][event.context.loop.index] = event.context.data[event.task.loop.output_element]
+                event.context.data[event.task.loop.output_collection][event.context.loop.index] = event.context.data[
+                    event.task.loop.output_element]
 
             return event.context
 
