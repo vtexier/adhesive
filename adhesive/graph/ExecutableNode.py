eb7c01de8b7c06a2de996d22dd87e1f461383298
Vincent Texier <vit@free.fr>
2021-05-14 13:46:03 +0200

    [fix] fix mypy errors on missing attributes

--- adhesive/graph/ExecutableNode.py
+++ adhesive/graph/ExecutableNode.py
@@ -1,5 +1,23 @@
+from typing import Optional
+
+from adhesive.execution.ExecutionData import ExecutionData
 from adhesive.graph.ProcessNode import ProcessNode
 
 
 class ExecutableNode(ProcessNode):
-    pass
+    def __init__(self, *args, id: str, name: str, parent_process: Optional['adhesive.graph.Process.Process']):
+        """
+        Init ExecutableNode
+
+        :param args:
+        :param id:
+        :param name:
+        :param parent_process:
+        """
+        super().__init__(*args, id=id, name=name, parent_process=parent_process)
+
+        self.loop = None
+        self.mapping = None
+        self.headers = {}
+        self.input = ExecutionData()
+        self.output = ExecutionData()
